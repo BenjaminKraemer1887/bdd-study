0e903a82fc3f8ce89052a631e6184fe633974697
benjamin <benjamin.kraemer@consid.com>
2024-05-15 20:40:30 +0200

    add slide funktion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SlideComponent} from "./app/component/slide/slide.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SlideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
