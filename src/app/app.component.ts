b5eca1bc95059117413cb8eec562a32e9fad19e2
benjamin <benjamin.kraemer@consid.com>
2024-05-17 15:05:26 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {SlideComponent} from "./app/component/slide/slide.component";
+import { SlideComponent } from './app/component/slide/slide.component';
+import { FooterComponent } from './app/component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SlideComponent],
+  imports: [RouterOutlet, SlideComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bdd-study';
